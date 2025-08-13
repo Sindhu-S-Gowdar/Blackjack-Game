372b142d318a4bbf16e475dce0f31dc390426027
Sindhu-S-Gowdar <sindhusgowdar77@gmail.com>
2025-08-13 13:40:18 +0530

    stage 1

--- Index.js
+++ Index.js
@@ -1,21 +1,44 @@
-let firstCard = 11;
-let secondCard = 10;
+let firstCard = 10;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
   if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card? ";
   } else if (sum === 21) {
-    message = "Wohooo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!! ";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😢";
+    message = "You're out of the game! ";
     isAlive = false;
   }
 
   messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  let card = 6;
+  sum += card;
+  cards.push(card);
+  console.log(cards);
+  renderGame();
 }
