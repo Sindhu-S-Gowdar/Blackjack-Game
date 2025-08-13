1fe96cbb8c304466e30fa31aa6f5712e80b87d6d
Sindhu-S-Gowdar <sindhusgowdar77@gmail.com>
2025-08-13 11:34:41 +0530

    messageEl

--- Index.js
+++ Index.js
@@ -4,6 +4,7 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
 
 function startGame() {
   if (sum < 21) {
@@ -16,5 +17,5 @@ function startGame() {
     isAlive = false;
   }
 
-  console.log(message);
+  messageEl.textContent = message;
 }
