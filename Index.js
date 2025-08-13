52b69e9d5229772f3ae14a9b7f784662f0194340
Sindhu-S-Gowdar <sindhusgowdar77@gmail.com>
2025-08-13 16:10:14 +0530

    final push

--- Index.js
+++ Index.js
@@ -1,16 +1,38 @@
-let firstCard = 10;
-let secondCard = 4;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  name: "Sindhu",
+  chips: 145,
+};
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandom() {
+  let number = Math.floor(Math.random() * 13) + 1;
+  if (number === 1) {
+    return 11;
+  } else if (number > 10) {
+    return 10;
+  } else {
+    return number;
+  }
+}
+
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandom();
+  let secondCard = getRandom();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
@@ -35,10 +57,11 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!");
-  let card = 6;
-  sum += card;
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandom();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+  }
 }
